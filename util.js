0c570a71e4cc6cc53b21626bfd78623fac5339b4
Krakenfall <krakenfall@gmail.com>
2018-03-27 22:51:29 -0700

    clean[logger]: Remove old logger

--- util.js
+++ util.js
@@ -5,22 +5,6 @@ var configService = require('./services/configuration.js');
 var db = require('./db.js');
 const logger = require('./services/log');
 
-var log = function(data, file, logInConsole) {
-	var logFile = "";
-	if (file) {
-		logFile = file;
-	} else {
-		logFile = "server.log";
-	}
-	try {
-		fs.appendFileSync(logFile, new Date() + "\r\n");
-		if (logInConsole) { console.log(data); }
-		fs.appendFileSync(logFile, data + "\r\n\r\n");
-	} catch (error) {
-		console.log("Error: Failed to log data in " + file + "\r\nData: " + data);
-	}
-};
-
 var getFileContents = function(filename, callback) {
 	var contents = null;
 	try {
